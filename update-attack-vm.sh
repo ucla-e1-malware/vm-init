59f05ced319986bdf01436088e6cfaf68da4110c
Arc-blroth <45273859+Arc-blroth@users.noreply.github.com>
2024-09-30 22:58:07 -0700

    update-attack-vm: i think this is correct bash

---
+++ update-attack-vm.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# update-vm.sh: a helper script to bootstrap the E1 Malware VM
+
+# become root (you should be able to understand this by the end of the course :D)
+if [ "$(id -u)" -ne 0 ]; then
+  pkexec $0
+  exit $?
+fi
+
+# returns true if the package $1 is not installed
+needs_install () {
+    [ "$(dpkg -l "$1" > /dev/null 2>&1; echo $?)" -eq 1 ]
+}
+
+apt update
+
+if needs_install software-properties-common; then
+    echo "⚙️ Installing software-properties-common..."
+    apt install software-properties-common
+fi
+
+if needs_install ansible || needs_install ansible-pull; then
+    echo "⚙️ Installing ansible..."
+    add-apt-repository --yes --update ppa:ansible/ansible
+    apt install ansible ansible-pull
+fi
+
+echo "⚙️ Pulling ansible config..."
+ansible-pull -U https://github.com/ucla-e1-malware/vm-init playbooks/attack.yaml
